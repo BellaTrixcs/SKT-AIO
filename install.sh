ff44d666b51d9315f88d0dcc3addf4e0993c5cd9
BellaTrixcs <skartistore@gmail.com>
2024-10-02 14:15:43 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -427,54 +427,9 @@ yes | sudo ufw enable
 # Make Other
 touch /etc/xray/proxy.log
 
-# Install NoobzVPN'S
-# [ Create Directory File ]
-mkdir -p /etc/noobzvpns
-
 # [ Membersihkan layar ]
 clear
 
-# [ Membuat Json Config yang di gunakan pada server ]
-cat > /etc/noobzvpns/config.json <<-JSON
-{
-	"tcp_std": [
-		8080
-	],
-	"tcp_ssl": [
-		8443
-	],
-	"ssl_cert": "/etc/noobzvpns/cert.pem",
-	"ssl_key": "/etc/noobzvpns/key.pem",
-	"ssl_version": "AUTO",
-	"conn_timeout": 60,
-	"dns_resolver": "/etc/resolv.conf",
-	"http_ok": "HTTP/1.1 101 Switching Protocols[crlf]Upgrade: websocket[crlf][crlf]"
-}
-JSON
-# Port Dari tcp_std & tcp_ssl edit sesuai kemauan kalian agar tidak bentrok dengan service lain pada vps kalian
-
-
-# [ wget ambil file ]
-wget -q -O /usr/bin/noobzvpns "https://github.com/noobz-id/noobzvpns/raw/master/noobzvpns.x86_64"
-wget -q -O /etc/noobzvpns/cert.pem "https://github.com/noobz-id/noobzvpns/raw/master/cert.pem"
-wget -q -O /etc/noobzvpns/key.pem "https://github.com/noobz-id/noobzvpns/raw/master/key.pem"
-
-
-# [ memberi izin pada file json & cert + key ]
-chmod +x /etc/noobzvpns/*
-
-# [ Memberi Izin Exec pada file biner ]
-chmod +x /usr/bin/noobzvpns
-
-# [ Mengambil Service yang di perlukan ]
-wget -q -O /etc/systemd/system/noobzvpns.service "https://github.com/noobz-id/noobzvpns/raw/master/noobzvpns.service"
-
-# [ Enable & Start Service ]
-systemctl enable noobzvpns
-systemctl restart noobzvpns
-
-# [ Membersihkan layar ]
-clear
 
 # Install Slowdns
 cd
@@ -871,7 +826,7 @@ URL="https://api.telegram.org/bot$KEY/sendMessage"
 curl -s --max-time $TIME --data-urlencode "chat_id=$CHATID" --data-urlencode "text=$OUTPUT" $URL >/dev/null 2>&1
 clear
 echo ""
-echo -e "\033[96m_______________________________\033[0m"
-echo -e "\033[92m  اَلْحَمْدُ لِلهِ رَبِّ اْلعَالَمِيْ \033[0m"
-echo -e "\033[96m_______________________________\033[0m"
+echo -e "\033[96m____________________________\033[0m"
+echo -e "\033[92m  اَلْحَمْدُ لِلهِ رَبِّ اْلعَالَمِيْ  \033[0m"
+echo -e "\033[96m____________________________\033[0m"
 sleep 1.5
